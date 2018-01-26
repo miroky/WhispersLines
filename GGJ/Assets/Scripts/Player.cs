774f628edce47f65b03651ee0668f333d49cc452
Rubén <r.carrascoizquierdo@gmail.com>
2018-01-26 23:36:03 +0100

    scripts y assets de ejemplo añadidos

---
+++ GGJ/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	//var
+	[SerializeField] private float speed;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetButtonDown("Fire1")){
+
+			RaycastHit hitInfo = new RaycastHit ();
+			bool hit = Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hitInfo);
+
+			if (hit) {
+			
+				Debug.Log (hitInfo.transform.gameObject.name);
+			} else {
+			
+				Debug.Log ("No hit");
+			}
+
+		}
+
+		if(Input.GetAxis("Horizontal") != 0){
+
+			transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime,0f,0f);
+		}
+	}
+}
