6c852a9e58060c2309184cce63e109954c7dbcbc
Varo Hurtado <ahs007@gmail.com>
2018-01-28 11:17:57 +0100

    Script de Animación de paloma (Incompleto)

---
+++ GGJ/Assets/Scripts/PalomaHaceController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalomaHaceController : MonoBehaviour {
+
+    public Animator palomaAterriza;
+    public Animator palomaAparece;
+
+    public AudioSource audio;
+
+    void Start()
+    {
+        palomaAterriza = GetComponent<Animator>();   
+    }
+
+    public void AterrizarPaloma()
+    {
+        palomaAterriza
+        palomaAparece.Play();
+
+        audio.Play();
+    }
+
+    public void DespegaPaloma()
+    {
+
+    }
+
+}
---
+++ GGJ/Assets/Scripts/PalomaVieneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PalomaHaceController : MonoBehaviour {
+
+    public Animation palomaAterriza;
+    public Animation palomaAparece;
+
+	public void AterrizarPaloma()
+    {
+
+    }
+
+    public void DespegaPaloma()
+    {
+
+    }
+}
